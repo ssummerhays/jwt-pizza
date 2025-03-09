a9115701c174ba8012fb4a38f5126eb16feabf96
ssummerhays <spencersummerhays3@gmail.com>
2025-03-08 19:40:57 -0700

    Retrying production deployment check

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -26,7 +26,6 @@ import { pizzaService } from '../service/service';
 import { Role, User } from '../service/pizzaService';
 import 'preline/preline';
 
-// Meaningless comment
 declare global {
   interface Window {
     HSStaticMethods: any;
